c6fd26361e9ae70a52412c42958b048e1833ab73
Rainã Pepe <raina.pepe@outlook.com>
2024-02-29 22:02:30 -0300

    add health checker

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,8 +1,25 @@
 use actix_web::{web, Scope};
+use actix_web::{HttpResponse, Responder};
+use serde::Serialize;
 
 pub mod quest;
+
+#[derive(Serialize)]
+struct HealthCheck {
+    status: String,
+    message: String
+}
+
+async fn health() -> impl Responder {
+    HttpResponse::Ok().json(HealthCheck {
+        status: String::from("OK"),
+        message: String::from("MHFrontier cq tool is alive!!")
+    })
+}
 
 pub fn get_all_routers() -> Scope {
     web::scope("")
         .service(quest::get_routers())
+        .route("/", web::get().to(health))
+        .route("/health", web::get().to(health))
 }
